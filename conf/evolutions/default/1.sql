776c6a5653367e057e18bf7e2e424f2c958688a7
yangrenhui <yangrenhui@yangrenhui.centos7>
2015-12-18 16:28:16 +0800

    udpate

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups
 create table `MachineLearnModel` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `sourceData` TEXT NOT NULL,
   `description` TEXT NOT NULL
