244e7f67ebae8fb341e1a33f0cecb9f1949dd500
yangrenhui <yangrenhui@yangrenhui.centos7>
2015-12-22 14:04:22 +0800

    update

--- app/actors/ProcessActor.scala
+++ app/actors/ProcessActor.scala
@@ -57,7 +57,7 @@ class ProcessActor(statesActor:ActorRef) extends Actor{
     val is = Hdfs.read(s"model/$modelName"+"_description")
     MachineLearnModels.add(MachineLearnModel(0,modelName,filename,scala.io.Source.fromInputStream(is).getLines.mkString("\n")))
     is.close
-    Hdfs.del(s"model/$modelName"+"_description")//model/111_description
+    Hdfs.del(s"model/$modelName"+"_description/part-00000")
     statesActor!s"Update $filename 100"
   }
 
